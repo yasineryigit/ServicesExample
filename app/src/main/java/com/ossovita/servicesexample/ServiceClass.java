929c25b26973fa015955e16e291fd0eb3e4e4e7b
sancaryum <sancaryum@gmail.com>
2020-12-30 01:33:41 +0300

    finished

--- app/src/main/java/com/ossovita/servicesexample/ServiceClass.java
+++ app/src/main/java/com/ossovita/servicesexample/ServiceClass.java
@@ -2,8 +2,10 @@ package com.ossovita.servicesexample;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
+import android.util.LogPrinter;
 
 import androidx.annotation.Nullable;
 
@@ -13,19 +15,29 @@ public class ServiceClass extends Service {
 
     @Override
     public void onCreate() {
-        Log.d(TAG,"onCreate");
+        String currentThread = Thread.currentThread().getName();
+        Log.d(TAG,"onCreate: "+currentThread);
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG,"onStartCommand");
+        String currentThread = Thread.currentThread().getName();
+        Log.d(TAG,"onStartCommand: "+currentThread);
+        /*try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        AsyncTaskingClass asyncTaskingClass = new AsyncTaskingClass();
+        asyncTaskingClass.execute(10000);
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
-        Log.d(TAG,"onDestroy");
+        String currentThread = Thread.currentThread().getName();
+        Log.d(TAG,"onDestroy: "+currentThread );
         super.onDestroy();
     }
 
@@ -33,5 +45,45 @@ public class ServiceClass extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    class AsyncTaskingClass extends AsyncTask<Integer,Void,Void>{
+
+        private static final String TAG = "AsyncTaskingClass";
+
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG,"doInBackground:"+ currentThread);
+            //Parametre olarak gelen süre boyunca bekleyecek
+            int sleepTime = integers[0];
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG,"onPreExecute:"+ currentThread);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG,"onPostExecute:"+ currentThread);
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            String currentThread = Thread.currentThread().getName();
+            Log.d(TAG,"onProgressUpdate:"+ currentThread);
+            super.onProgressUpdate(values);
+        }
     }
 }
